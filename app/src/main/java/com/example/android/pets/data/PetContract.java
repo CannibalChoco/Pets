cc0a09d6846e7dc69fb60c91685335f126c6fd67
CannibalChoco <fudgeblimp@gmail.com>
2017-07-05 15:34:44 +0300

    Added URIs to the Contract

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,6 +1,7 @@
 package com.example.android.pets.data;
 
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 // Class holds only constants, so we won't need to extend it
@@ -8,7 +9,13 @@ public final class PetContract {
 
     private PetContract(){}
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     public static abstract class PetEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public static final String TABLE_NAME = "pets";
         public static final String _ID = BaseColumns._ID;
